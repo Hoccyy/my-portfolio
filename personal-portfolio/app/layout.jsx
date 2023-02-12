a3edd6863308ead5811e3ebefa38f6f58ae34281
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-02-12 01:31:36 -0500

    New animations, svg logos, and skillsets

--- personal-portfolio/app/layout.jsx
+++ personal-portfolio/app/layout.jsx
@@ -13,13 +13,21 @@ export default function RootLayout({ children }) {
         <div class="left-tab">
           <ul class='menu-it'>
             <li class='rightm'>
-              <span className={styles.card}><p className={styles.description}>About</p></span>
-              <span className={styles.card}><a target="blank" href='https://twitter.com/Hoccyy_' className={styles.description}>Twitter</a></span>
-              <span className={styles.card}><a target="blank" href='https://github.com/Hoccyy' className={styles.description}>Github</a></span></li> 
+              <span className={styles.card}><p className={styles.descriptionL}>About</p></span>
+              <span className={styles.card}><a target="blank" href='https://twitter.com/Hoccyy_' className={styles.descriptionL}>Twitter</a></span>
+              <span className={styles.card}><a target="blank" href='https://github.com/Hoccyy' className={styles.descriptionL}>Github</a></span></li> 
           </ul>
         </div>
         <div>
           {children}
+        </div>
+        <div className={styles.footer}>
+          <ul>
+            <li className={styles.description}>
+            Contact  
+            </li>  
+            <li className={styles.title2GI}>@Hoccyy</li>  
+          </ul>
         </div>
       </body>
     </html>
--- personal-portfolio/app/page.jsx
+++ personal-portfolio/app/page.jsx
@@ -20,32 +20,188 @@ export default function Home() {
           />
           <div className={styles.sizefit}><h1 className={styles.title}>Hocian Wade.</h1></div>
           <div id='content-0'>
-          <h1 className={styles.title2}>I am a Full-stack developer</h1></div>
-          <span>
-            <p className={styles.title3}>About me,</p>
-            <p className={styles.description1}>I'm a university student currently enrolled in Computer Science at York University.</p>
-          </span>
+          <h1 className={styles.title2FS}>I am a Full-stack developer</h1></div>
 
         </div>{/*Div1*/}
-        <div className='content-2'>
+
+
+
+        <div className={styles.content3}>
           {/*<button className={styles.button}>Resume</button>*/}
-          <div id='content-0'> <h3 className={styles.title2}>My Projects</h3> </div>
-          <ul className='banners'>
-              <li className='banners-1'><h1 className={styles.description}>Morse-Code Translator</h1>
-              <h3 className={styles.description}>A web application that translates english into morsecode and vice versa</h3>
-              </li>
-              <li className='banners-1'><h1 className={styles.description}>Python Cache cleaner</h1></li>
-              <li className='banners-1'><h1 className={styles.description}>Trivia Game</h1></li>
-          </ul>
-          <ul className='banners'>
-              <li className='banners-1'><h1 className={styles.description}>Arduino Timer</h1></li>
-              <li className='banners-1'><h1 className={styles.description}>Water Level Tracker</h1>
-              <h2 className={styles.wipTitle}>Coming soon</h2>
-              </li>
-              <li className='banners-1'><h1 className={styles.description}>Weather App</h1>
-              <h2 className={styles.wipTitle}>Coming soon</h2>
-              </li>
-          </ul>
+          <div id='content-0'> <h3 className={styles.title2Sk}>My Skills</h3> </div>
+
+              <div className={styles.banGrid}>{/*Skill set 1*/}
+                <div className={styles.gridSkills}>
+                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/Python.svg"
+                  alt="Python 3.0 Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+                <div className={styles.gridSkillsJS}>
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/JavaScript.svg"
+                  alt="JavaScript(Vanilla JS) Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+                <div className={styles.gridSkills}>
+                <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/Java.svg"
+                  alt="Java Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+              </div>
+
+              <div className={styles.banGrid}>{/*Skillset 2 */}
+                <div className={styles.gridSkillsN}>
+                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/next.svg"
+                  alt="Next.JS Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+                <div className={styles.gridSkills}>
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/React.svg"
+                  alt="React Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+                <div className={styles.gridSkills}>
+                <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/Csharp.svg"
+                  alt="C# Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+              </div>
+              <div className={styles.banGrid}>{/*Skillset 3 */}
+                <div className={styles.gridSkillsJS}>
+                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/Html.svg"
+                  alt="HTML Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+                <div className={styles.gridSkillsJS}>
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/CSS.svg"
+                  alt="CSS Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+                <div className={styles.gridSkills}>
+                <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/CPP.svg"
+                  alt="C++(CPP) Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+              </div>
+              <div className={styles.banGrid}>{/*Skillset 3 */}
+                <div className={styles.gridSkillsN}>
+                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
+                  <Image
+                  //src="/Python-logo.svg"
+                  src="/assets/SQL.svg"
+                  alt="SQL Logo"
+                  className={styles.skillLogo}
+                  width={100}
+                  height={24}
+                  draggable={false}
+                  priority
+                  />
+                </div>
+              </div>
+
+          </div>
+
+          <div className={styles.content2}>
+          <div id='content-0'> <h3 className={styles.title2Sk}>My Projects</h3> </div>
+          <div className={styles.banGrid}>
+            <div className={styles.gridItem}><h1 className={styles.title2}>Morse Translator</h1>
+            <Image
+            src="/assets/Morse.png"
+            alt="Vercel Logo"
+            className={styles.bumpLogo}
+            width={100}
+            height={24}
+            draggable={false}
+            priority
+            />
+            <h3 className={styles.title2GI}>A web application to translate English into Morse Code and vice versa</h3>
+          </div>
+            <div className={styles.gridItem}><h1 className={styles.title2}>Cache-Woosh</h1></div>
+            <div className={styles.gridItem}><h1 className={styles.title2}>Arduino Timer</h1></div>
+            <div className={styles.gridItem}><h1 className={styles.title2}>Weather app</h1></div>
+          </div>
+          <div className={styles.banGrid}>
+            <div className={styles.gridItem}><h1 className={styles.title2}>Water Tracker</h1>
+            <Image
+            src="/assets/Morse.png"
+            alt="Vercel Logo"
+            className={styles.bumpLogo}
+            width={100}
+            height={24}
+            draggable={false}
+            priority
+          />
+            </div>
+            <div className={styles.gridItem}><h1 className={styles.title2}>Cache Cleaner</h1></div>
+            <div className={styles.gridItem2}><h1 className={styles.title2}>Cache Cleaner</h1></div>
+            <div className={styles.gridItem2}><h1 className={styles.title2}>Cache Cleaner</h1></div>
+
+          </div>
           <h1 className={styles.title2}>Something</h1>
         </div>
 

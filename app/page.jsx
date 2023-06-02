9bafaf428b0f64eff3ccd8b250774def362fe6ba
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-06-02 17:45:37 -0400

    Structure changes and unnecessary comment removal

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import { Inter } from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter ({subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -18,7 +18,6 @@ export default function Home() {
 
         </div>{/*Div1*/}
 
-
         <div className={styles.content3}>
           
           <div id='content-0'> <h3 className={styles.title2Sk}>Resume</h3> </div>
@@ -27,20 +26,17 @@ export default function Home() {
           alt="Resume"
           className={styles.resume}
           width={690}
-          height={1300}
+          height={1450}
           draggable={true}
           priority
           />
         </div>
         <div className={styles.content3}>
-          {/*<button className={styles.button}>Resume</button>*/}
           <div id='content-0'> <h3 className={styles.title2Sk}>My Skills</h3> </div>
 
-              <div className={styles.banGrid}>{/*Skill set 1*/}
+              <div className={styles.banGrid}>
                 <div className={styles.gridSkills}>
-                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
                   <Image
-                  //src="/Python-logo.svg"
                   src="/assets/Python.svg"
                   alt="Python 3.0 Logo"
                   className={styles.skillLogo}
@@ -64,7 +60,6 @@ export default function Home() {
                 </div>
                 <div className={styles.gridSkills}>
                 <Image
-                  //src="/Python-logo.svg"
                   src="/assets/Java.svg"
                   alt="Java Logo"
                   className={styles.skillLogo}
@@ -76,11 +71,9 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className={styles.banGrid}>{/*Skillset 2 */}
+              <div className={styles.banGrid}>
                 <div className={styles.gridSkillsN}>
-                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
                   <Image
-                  //src="/Python-logo.svg"
                   src="/assets/next.svg"
                   alt="Next.JS Logo"
                   className={styles.skillLogo}
@@ -92,7 +85,6 @@ export default function Home() {
                 </div>
                 <div className={styles.gridSkills}>
                   <Image
-                  //src="/Python-logo.svg"
                   src="/assets/React.svg"
                   alt="React Logo"
                   className={styles.skillLogo}
@@ -104,7 +96,6 @@ export default function Home() {
                 </div>
                 <div className={styles.gridSkills}>
                 <Image
-                  //src="/Python-logo.svg"
                   src="/assets/Csharp.svg"
                   alt="C# Logo"
                   className={styles.skillLogo}
@@ -115,11 +106,9 @@ export default function Home() {
                   />
                 </div>
               </div>
-              <div className={styles.banGrid}>{/*Skillset 3 */}
+              <div className={styles.banGrid}>
                 <div className={styles.gridSkillsJS}>
-                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
                   <Image
-                  //src="/Python-logo.svg"
                   src="/assets/Html.svg"
                   alt="HTML Logo"
                   className={styles.skillLogo}
@@ -152,9 +141,10 @@ export default function Home() {
                   />
                 </div>
               </div>
-              <div className={styles.banGrid}>{/*Skillset 3 */}
+
+              <div className={styles.banGrid}>
                 <div className={styles.gridSkillsN}>
-                  {/*<h1 className={styles.title2Sk}>Python</h1>*/}
+
                   <Image
                   //src="/Python-logo.svg"
                   src="/assets/SQL.svg"
@@ -165,6 +155,7 @@ export default function Home() {
                   draggable={false}
                   priority
                   />
+                    
                 </div>
               </div>
 
@@ -174,6 +165,7 @@ export default function Home() {
           <div id='content-0'> <h3 className={styles.title2Sk}>My Projects</h3> </div>
           <div className={styles.banGrid}>
               <div className={styles.gridItem}>
+                
                 <Image
                 src="/assets/Morse2.svg"
                 alt="Vercel Logo"
@@ -183,6 +175,7 @@ export default function Home() {
                 draggable={false}
                 priority
                 />
+                  
                 <a target="_blank" href='https://www.morseley.com/'>
                   <h1 className={styles.title2GI}>Morse Translator</h1>
                 </a>
@@ -191,6 +184,7 @@ export default function Home() {
                 </div>
             </div>
               <div className={styles.gridItem}>
+
                 <Image
                 src="/assets/CacheW.svg"
                 alt="Vercel Logo"
@@ -200,6 +194,7 @@ export default function Home() {
                 draggable={false}
                 priority
                 />
+
                 <a target="_blank" href='https://github.com/Hoccyy/Cache-Whoosh'>
                   <h1 className={styles.title2GI}>Cache-Woosh</h1>
                 </a>
@@ -209,6 +204,7 @@ export default function Home() {
             </div>
 
             <div className={styles.gridItem}>
+              
               <Image
                 src="/assets/wip.svg"
                 alt="Vercel Logo"
@@ -218,6 +214,7 @@ export default function Home() {
                 draggable={false}
                 priority
               />
+                  
               <a target="_blank" href='https://github.com/Hoccyy/Arduino-Timer-Display'>
                 <h1 className={styles.title2GI}>Arduino Timer</h1>
               </a>
@@ -227,6 +224,7 @@ export default function Home() {
             </div>
 
             <div className={styles.gridItem}>
+              
               <Image
                 src="/assets/wip.svg"
                 alt="Vercel Logo"
@@ -236,6 +234,7 @@ export default function Home() {
                 draggable={false}
                 priority
               />
+                  
               <a target="_blank" href='https://chrome.google.com/webstore/detail/mirrorhound/iapfedepagfopcakfpejjjlkafmnpoeh'>
                  <h1 className={styles.title2GI}>MirrorHound</h1>
               </a>
@@ -246,6 +245,7 @@ export default function Home() {
           </div>
           <div className={styles.banGrid}>
             <div className={styles.gridItem}>
+              
               <Image
                 src="/assets/wip.svg"
                 alt="Vercel Logo"
@@ -255,6 +255,7 @@ export default function Home() {
                 draggable={false}
                 priority
               />
+                  
               <h1 className={styles.title2GI}>Water Tracker</h1>
               <div className={styles.textCont}>
                 <h3 className={styles.description2}>Web app to track daily water intake written using Next.JS</h3>
@@ -262,6 +263,7 @@ export default function Home() {
             </div>
 
             <div className={styles.gridItem}>
+              
               <Image
                 src="/assets/QuickClick.svg"
                 alt="Vercel Logo"
@@ -271,6 +273,7 @@ export default function Home() {
                 draggable={false}
                 priority
               />
+                  
               <a target="_blank" href='https://github.com/Hoccyy/auto-clicker'>
                 <h1 className={styles.title2GI}>QuickClick</h1>
               </a>
@@ -278,22 +281,17 @@ export default function Home() {
                   <h3 className={styles.description2}>A python app made to automate repetitive clicking automatically. This can be used for games, or to help with your workflow and productivity.</h3>
               </div>
             </div>
-
-            <div className={styles.gridItem2}><h1 className={styles.title2}>Cache Cleaner</h1></div>
-            <div className={styles.gridItem2}><h1 className={styles.title2}>Cache Cleaner</h1></div>
-
+            <div className={styles.gridItem2}> <h1 className={styles.title2}>Cache Cleaner</h1> </div>
+            <div className={styles.gridItem2}> <h1 className={styles.title2}>Cache Cleaner</h1> </div>
           </div>
-          
-
           
           <div id='content-0'>
             <div className={styles.content4}>
               <h1 className={styles.title2Sk}>Education</h1>
-              <div className={styles.banGridEdu}>{/*Skill set 1*/}
+              <div className={styles.banGridEdu}>
                   <div className={styles.gridEdu}>
-                    {/*<h1 className={styles.title2Sk}>Python</h1>*/}
+
                     <Image
-                    
                     src="/assets/York.svg"
                     alt="Python 3.0 Logo"
                     className={styles.skillLogo}
@@ -302,11 +300,11 @@ export default function Home() {
                     draggable={false}
                     priority
                     />
+
                     </div> {/**Close Logo */}
                   <div className={styles.gridEdu}>
-                    {/*<h1 className={styles.title2Sk}>Python</h1>*/}
-                    <Image
                     
+                    <Image
                     src="/assets/AlgomaUniversity.svg"
                     alt="Python 3.0 Logo"
                     className={styles.skillLogo}
@@ -315,11 +313,10 @@ export default function Home() {
                     draggable={false}
                     priority
                     />
-                    </div> {/**Close Logo */}
+                      
+                    </div>
                   <div className={styles.gridEdu}>
-                    {/*<h1 className={styles.title2Sk}>Python</h1>*/}
                     <Image
-                    //src="/Python-logo.svg"
                     src="/assets/ThistletownCollegiateInstitute.svg"
                     alt="Python 3.0 Logo"
                     className={styles.skillLogo}
@@ -328,15 +325,11 @@ export default function Home() {
                     draggable={false}
                     priority
                     />
-                    </div> {/**Close Logo */}
+                    </div>
               </div>
-            
             </div>
-
           </div>
-
         </div>
-
 
     </div>{/*Main DIv*/}
     </main>

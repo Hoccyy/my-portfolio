344ac8fca82052f21f5dd8042f623daa6cc54deb
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2024-01-13 00:33:55 -0500

    Insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 import pageTitle from './page'
 import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -40,6 +40,7 @@ export default function RootLayout({
         </nav>
         </div>
         {children}
+        <SpeedInsights />
         <Analytics />
         <Footer/>
       </body>

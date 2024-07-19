f32f721d5f4ed4c580df20db380b7e3d839cb078
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2024-07-18 21:22:02 -0400

    Resume update     (Automated)

--- src/app/components/FetchKey.tsx
+++ src/app/components/FetchKey.tsx
@@ -2,5 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const FetchAPIKey = (): string | undefined => {
+    console.log('$30',process.env)
+    console.log('$30',process.env.FORM_KEY)
     return process.env.FORM_KEY;
 };
